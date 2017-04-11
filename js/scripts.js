0aa06cf8c4db0efe0b7d3ac2337ded29f38f98d9
Viv-Iam <vivian.opondoh@gmail.com>
2017-04-11 07:50:50 +0300

    Added jquery
    Used jquery to hide form

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// $(document).ready(function(){
+
+// });
+$(document).ready(function() {
+
+    $("button").click(function() {
+
+        $(".form-group").hide();
+        $("button").hide();
+        $("p").show();
+        alert("Data succesfully submitted!");
+    });
+
+});
